47727fa2f99d0cecc5fdc4dabf8aeb4c797c53e0
RamadhanIY <ihsani.yulfa@gmail.com>
2025-05-31 23:35:23 +0700

    fix: update QuizParticipant model to correct related_name for quiz field

--- app/models/models.py
+++ app/models/models.py
@@ -65,7 +65,7 @@ class Question(Model):
 class QuizParticipant(Model):
     id = fields.IntField(pk=True)
     user = fields.ForeignKeyField("models.User", related_name="participants")
-    quiz = fields.ForeignKeyField("models.Quiz", related_name="quiz")
+    quiz = fields.ForeignKeyField("models.Quiz", related_name="participants")
     status = fields.CharEnumField(StatusType)
     score = fields.IntField(default=0)
     joined_at = fields.DatetimeField(auto_now_add=True)
